f7babddf57addd413afbdf408f1fb4e5bfb49909
cyberpunknomads <90918872+cyberpunknomads@users.noreply.github.com>
2022-09-23 12:27:56 +0200

    Only CIS module

--- main.tf
+++ main.tf
@@ -1,25 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-north-1"
-}
-
-resource "aws_iam_account_password_policy" "CIS-Standard" {
-  minimum_password_length        = var.minimum_password_length
-  require_lowercase_characters   = var.require_lowercase_characters
-  require_numbers                = var.require_numbers
-  require_uppercase_characters   = var.require_uppercase_characters
-  require_symbols                = var.require_symbols
-  allow_users_to_change_password = var.allow_users_to_change_password
-  password_reuse_prevention      = var.password_reuse_prevention
-  max_password_age               = var.max_password_age
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 14
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  password_reuse_prevention      = 24
+  max_password_age               = 90
 }
